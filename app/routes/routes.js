25de75b2575b074927c2cabaddda34b4c39d9ec7
cgmora12 <cgmora12@hotmail.com>
2019-04-05 13:24:34 +0200

    Querying a SPARQL endpoint

--- app/routes/routes.js
+++ app/routes/routes.js
@@ -1,10 +1,34 @@
 // routes/routes.js
 
+const http 		 = require('http');
+
 module.exports = function(app, db) {
 	app.get('/UniversalAPI', (req, res) => {
 		console.log("params: " + JSON.stringify(req.query))
+		console.log("params: " + JSON.stringify(req.query.endpoint))
+
+		//TODO: query the endpoint to get possible paths or generate the SPARQL query from users query
+		http.get(req.query.endpoint + '?default-graph-uri=&query=select+distinct+%3FConcept+where+%7B%5B%5D+a+%3FConcept%7D&format=application%2Fsparql-results%2Bjson&debug=on&timeout=', (resp) => {
+		  let data = '';
+
+		  // A chunk of data has been recieved.
+		  resp.on('data', (chunk) => {
+		    data += chunk;
+		  });
+
+		  // The whole response has been received. Print out the result.
+		  resp.on('end', () => {
+		  	//console.log("data: " + data);
+		  	//res.send('API to LOD -> OK <br><br>'+ JSON.stringify(data))
+		  	res.json({a: JSON.parse(data)})
+		  });
+
+		}).on("error", (err) => {
+		  console.log("Error: " + err.message);
+		  res.send('API to LOD -> Error')
+		});
 
 		// Return data to users
-		res.send('API to LOD')
+		//
 	});
 };
