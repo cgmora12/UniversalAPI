d2ec4bd14476ea1f400566bb680cbf4b6dd41b08
cgmora12 <cgmora12@hotmail.com>
2019-05-23 14:04:25 +0200

    Return format fix

--- server.js
+++ server.js
@@ -10,7 +10,6 @@ require('./app/routes')(app, {});
 //app.use(bodyParser.urlencoded({ extended: true }));
 // Configurar cabeceras y cors
 app.use((req, res, next) => {
-    res.setHeader('Content-Type', 'application/json');
     res.header('Access-Control-Allow-Origin', '*');
     res.header('Access-Control-Allow-Headers', 'Authorization, X-API-KEY, Origin, X-Requested-With, Content-Type, Accept, Access-Control-Allow-Request-Method');
     res.header('Access-Control-Allow-Methods', 'GET, POST, OPTIONS, PUT, DELETE');
